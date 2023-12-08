695aa31935b440ce7c8563db3aa5c4940ab23736
kebean <chenqiua@gmail.com>
2023-12-08 03:00:36 +0200

    added marking important task_feature

--- app.js
+++ app.js
@@ -1,87 +1,80 @@
-const form = document.querySelector("#todo-form");
-const todoList = document.querySelector(".todos");
-const remaingTasks = document.querySelector("#remaining-tasks");
-const completedTasks = document.querySelector("#completed-tasks");
-const totalTasks = document.querySelector("#total-tasks");
-const inputTasks = document.querySelector("#todo-form input");
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-if(localStorage.getItem("tasks")){
-    tasks.map(task => createTask(task));
-}
-form.addEventListener("submit", e => {
-    e.preventDefault();
-    let inputTask = inputTasks.value;
-    if(inputTask == ""){
-        return;
-    }
-    const task = {
-        id: new Date().getTime(),
-        name : inputTask,
-        isCompleted: false,
-    }
-    tasks.push(task);
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    createTask(task);
-    form.reset();
-    inputTasks.focus();
-})
+document.addEventListener('DOMContentLoaded', function () {
+    // Check local storage for saved tasks
+    const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    renderTasks(savedTasks);
+});
 
-function createTask(task){
-    let taskElem = document.createElement("li");
-    taskElem.setAttribute("id", task.id);
-    if(task.isCompleted){
-        taskElem.classList.add("complete");
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== '') {
+        const task = {
+            text: taskText,
+            important: false,
+        };
+
+        // Retrieve tasks from local storage
+        const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+        // Add new task
+        tasks.unshift(task);
+
+        // Save tasks to local storage
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+
+        // Render updated tasks
+        renderTasks(tasks);
+
+        // Clear input field
+        taskInput.value = '';
     }
-    let domMarkUp = `
-    <div>
-    <input type="checkbox" name="tasks" id="${task.id}" ${task.isCompleted? "checked": ""}>
-    <span ${!task.isCompleted? "contenteditable" : ""}> ${task.name}</span>
-    </div>
-    <button title="Remove the "${task.name}" task " class="remove-task">Delete</button>
-    `
-    taskElem.innerHTML = domMarkUp;
-    todoList.appendChild(taskElem);
-    keepOfTasks();
-}
-function keepOfTasks(){
-    const completdTaskArr = tasks.filter(task => task.isCompleted == true);
-    totalTasks.textContent = tasks.length;
-    completedTasks.textContent = completdTaskArr.length;
-    remaingTasks.textContent = tasks.length - completdTaskArr.length;
 }
 
-todoList.addEventListener("click", e =>{
-    if(e.target.classList.contains("remove-task")){
-        let taskId = e.target.closest("li").id;
-        deleteTask(taskId);
-    }
-})
-function deleteTask(id){
-    tasks = tasks.filter(task => task.id !== parseInt(id));
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    document.getElementById(id).remove();
-    keepOfTasks();
+function deleteTask(index) {
+    // Retrieve tasks from local storage
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    // Remove task at the specified index
+    tasks.splice(index, 1);
+
+    // Save updated tasks to local storage
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    // Render updated tasks
+    renderTasks(tasks);
 }
-todoList.addEventListener("input", e =>{    
-    let taskId = e.target.closest("li").id;
-    updateTask(taskId, e.target)
-})
-function updateTask(id, el){
-    const task = tasks.find(task => task.id == parseInt(id));
-    if(el.hasAttribute("contenteditable")){
-        task.name = el.textContent;
-    }else{
-        let span = el.nextElementSibling
-        let par = el.closet("li");
-        task.isCompleted = !task.isCompleted
-        if(task.isCompleted){
-            span.removeAttribute("contenteditable")
-            par.classList.add("complete")
-        }else{
-            span.setAttribute("contenteditable")
-            par.classList.remove("complete")
-        }
-    }
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    keepOfTasks();
+
+function toggleImportant(index) {
+    // Retrieve tasks from local storage
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    // Toggle the 'important' status of the task at the specified index
+    tasks[index].important = !tasks[index].important;
+
+    // Save updated tasks to local storage
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    // Render updated tasks
+    renderTasks(tasks);
+}
+
+function renderTasks(tasks) {
+    const taskList = document.getElementById('taskList');
+
+    // Clear existing tasks
+    taskList.innerHTML = '';
+
+    // Render each task
+    tasks.forEach((task, index) => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `
+            <span style="${task.important ? 'color: red;' : ''}">${task.text}</span>
+            <div>
+                <button onclick="toggleImportant(${index})">Toggle Important</button>
+                <button onclick="deleteTask(${index})">Delete</button>
+            </div>
+        `;
+        taskList.appendChild(listItem);
+    });
 }
